93d4fd4a0272802fa1dcba08bbd99ee33a696172
neri78 <dikehara@gmail.com>
2021-06-10 11:04:11 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+console.log(`
+Daizen Ikehara
+
+Twitter:    https://twitter.com/neri78
+GitHub:     https://github.com/neri78
+Dev.to:     https://dev.to/neri78
+Twitch:     https://www.twitch.tv/neri78
+YouTube:    https://www.youtube.com/channel/UCaR5kVaCBLKSJlNgCuWPkDw
+LinkedIn:   https://www.linkedin.com/in/daizenikehara/
+Website:    https://www.pondfield.com
+
+Please contact me with one of channels above =)`);
